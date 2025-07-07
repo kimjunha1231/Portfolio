65e824570fca90e41fd5d714a31c87a5e30e551d
kimjunha1231 <tto9011@naver.com>
2025-07-08 02:23:54 +0900

    [Feat]: 모바일 화면 깨짐 수정

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,7 @@
 // 프로젝트 데이터를 consts.ts의 PROJECTS와 일치시킴
 const projectsData = {
   1: {
-    title: "Dearfam : 가족의 이야기를 담아드립니다",
+    title: "Dearfam",
     period: "2024.11 ~ 현재",
     role: `
 			<ul>
